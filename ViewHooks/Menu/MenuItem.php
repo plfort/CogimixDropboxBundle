e85014ecba1188f23692c5d44bfc177a7333a741
plfort <plfort@cogipix.com>
2014-03-09 23:30:43 +0100

    new menu

--- ViewHooks/Menu/MenuItem.php
+++ ViewHooks/Menu/MenuItem.php
@@ -1,17 +1,25 @@
 <?php
 namespace Cogipix\CogimixDropboxBundle\ViewHooks\Menu;
 use Cogipix\CogimixCommonBundle\ViewHooks\Menu\MenuItemInterface;
+use Cogipix\CogimixCommonBundle\ViewHooks\Menu\AbstractMenuItem;
 
 /**
  *
  * @author plfort - Cogipix
  *
  */
-class MenuRenderer implements MenuItemInterface{
+class MenuItem  extends AbstractMenuItem{
 
     public function getMenuItemTemplate()
     {
           return 'CogimixDropboxBundle:Menu:menu.html.twig';
 
     }
+
+    public function getName(){
+    	return 'dropbox';
+    }
+
+
+
 }
