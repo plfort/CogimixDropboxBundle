e725918e0138b41219c0910d6e47e20e86d792ae
plfort <plfort@cogipix.com>
2016-04-20 18:56:00 +0200

    Posgtresql migration

--- Entity/AccessToken.php
+++ Entity/AccessToken.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @author plfort - Cogipix
  * @ORM\Entity
+ * @ORM\Table(name="accesstoken")
  */
 class AccessToken
 {
@@ -14,7 +15,7 @@ class AccessToken
     /**
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
     /**
